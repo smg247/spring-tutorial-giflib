d486ecb1e744338225c019f0e4f15c083a1b6c39
Stephen Goeddel <stephen.goeddel@benefitfocus.com>
2017-09-19 11:29:02 -0400

    repository for gifs created.

--- src/main/java/com/stephengoeddel/giflib/controller/GifController.java
+++ src/main/java/com/stephengoeddel/giflib/controller/GifController.java
@@ -1,11 +1,19 @@
 package com.stephengoeddel.giflib.controller;
 
+import com.stephengoeddel.giflib.data.GifRepository;
+import com.stephengoeddel.giflib.model.Gif;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.time.LocalDate;
+
 @Controller
 public class GifController {
+
+    @Autowired private GifRepository gifRepository;
 
     @RequestMapping(value = "/")
     public String listGifs() {
@@ -13,8 +21,9 @@ public class GifController {
     }
 
     @RequestMapping(value = "/gif")
-    @ResponseBody
-    public String gifDetail() {
-        return "This is the detail for a GIF.";
+    public String gifDetails(ModelMap modelMap) {
+        Gif gif = gifRepository.findByName("android-explosion");
+        modelMap.put("gif", gif);
+        return "gif-details";
     }
 }
---
+++ src/main/java/com/stephengoeddel/giflib/data/GifRepository.java
@@ -0,0 +1,28 @@
+package com.stephengoeddel.giflib.data;
+
+import com.stephengoeddel.giflib.model.Gif;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Repository
+public class GifRepository {
+    private static final List<Gif> ALL_GIFS = Arrays.asList(
+            new Gif("android-explosion", LocalDate.of(2015,2,13), "Chris Ramacciotti", false),
+            new Gif("ben-and-mike", LocalDate.of(2015,10,30), "Ben Jakuben", true),
+            new Gif("book-dominos", LocalDate.of(2015,9,15), "Craig Dennis", false),
+            new Gif("compiler-bot", LocalDate.of(2015,2,13), "Ada Lovelace", true),
+            new Gif("cowboy-coder", LocalDate.of(2015,2,13), "Grace Hopper", false),
+            new Gif("infinite-andrew", LocalDate.of(2015,8,23), "Marissa Mayer", true)
+    );
+    
+    public Gif findByName(String name) {
+        return ALL_GIFS.stream()
+                .filter(gif -> gif.getName().equals(name))
+                .findFirst()
+                .orElse(null);
+    }
+}
